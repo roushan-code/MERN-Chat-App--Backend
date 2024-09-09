21d3a65effe3bb80027a2107c0be02d95471bd06
Roushan <roushansingh3698@gmail.com>
2024-09-09 23:09:53 +0530

    url confirm

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-        origin: ["http://localhost:5173", "http://localhost:4173", process.env.CLIENT_URL],
+        origin: [  process.env.CLIENT_URL, "http://localhost:5173",],
         credentials: true,
     }
 
